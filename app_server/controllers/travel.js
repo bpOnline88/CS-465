b48103078fb803b57c219e31cc8a0904244bea42
Bryce Pangburn <brycepangburn88@gmail.com>
2024-03-24 16:50:32 -0400

    Module 3 Completed Baseline

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,4 +1,7 @@
-/* GET Homepage */
+var fs = require('fs');
+var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+
+/* GET travel view */
 const travel = (req, res) => {
     res.render('travel', {title: "Travlr Getaways"});
 };
